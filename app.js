7947791cc5f25d46970f04663997647e25f79b20
Patrick Drejerczak <drejerczak@aol.com>
2021-12-09 14:19:36 +0100

    changed .env

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const mongoose = require("mongoose");
 const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose
-  .connect(MONGODB_URI, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
 
     useUnifiedTopology: true,
