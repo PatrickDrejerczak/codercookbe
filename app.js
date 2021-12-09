a80ffe53f9aa4e82e424c55eeefd5c748ba7df82
Patrick Drejerczak <drejerczak@aol.com>
2021-12-09 14:23:40 +0100

    revert change

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const mongoose = require("mongoose");
 const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(MONGODB_URI, {
     useNewUrlParser: true,
 
     useUnifiedTopology: true,
